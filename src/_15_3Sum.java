088a9332f7fa246bf00e1c9ddc43fc2383a4a438
pingcai <11867939@qq.com>
2017-05-10 21:29:02 +0800

    三数和

---
+++ src/_15_3Sum.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by pingcai on 17-4-27.
+ */
+public class _15_3Sum {
+
+    public static void main(String[] args) {
+        int[] a = new int[]{-1, 0, 1, 2, -1, -4};
+        System.out.println(new _15_3Sum().threeSum(a));
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return list;
+        }
+        int i = 0;
+        Arrays.sort(nums);
+        while (i < nums.length - 2 && nums[i] <= 0) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                i++;
+                continue;
+            }
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int x = nums[i] + nums[j] + nums[k];
+                if (x == 0) {
+                    list.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                } else if (x < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+            i++;
+        }
+        return list;
+    }
+}
---
+++ src/_16_3SumClosest.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+/**
+ * Created by pingcai on 17-4-29.
+ * <p>
+ * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target.
+ * Return the sum of the three integers. You may assume that each input would have exactly one solution.
+ * For example, given array S = {-1 2 1 -4}, and target = 1.
+ * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ * <p>
+ * 思路：
+ * 先排序再枚举，找出和绝对值最接近target的三个数
+ */
+
+public class _16_3SumClosest {
+
+    public int threeSumClosest(int[] nums, int target) {
+
+        int len = nums.length;
+        int res = nums[0] + nums[1] + nums[len - 1];
+        int j = 0;
+        int k = 0;
+        int x = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < len - 2; i++) {
+            j = i + 1;
+            k = len - 1;
+            while (j < k) {
+                x = nums[i] + nums[j] + nums[k];
+                if (x > target) {
+                    k--;
+                } else if (x < target) {
+                    j++;
+                } else {
+                    return x;
+                }
+                if (Math.abs(target - x) < Math.abs(target - res)) {
+                    res = x;
+                }
+            }
+
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[]{13, 2, 0, -14, -20, 19, 8, -5, -13, -3, 20, 15, 20, 5, 13, 14, -17, -7, 12, -6, 0, 20, -19, -1, -15, -2, 8, -2, -9, 13, 0, -3, -18, -9};
+        //int[] a = new int[]{0,2,1,-3};
+        System.out.println(new _16_3SumClosest().threeSumClosest(a, -59));
+    }
+}
