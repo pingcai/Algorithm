56b0e8c9231c5ed147c8da7d30f4d44e81f0660f
pingcai <11867939@qq.com>
2017-07-07 20:03:46 +0800

    数组去重

--- src/leetcode/_26_Remove_Duplicates_from_Sorted_Array.java
+++ src/leetcode/_26_Remove_Duplicates_from_Sorted_Array.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 /**
  * Created by pingcai on 17-7-7.
  */
-public class _26Remove_Duplicates_from_Sorted_Array {
+public class _26_Remove_Duplicates_from_Sorted_Array {
     public static void main(String[] args) {
         int[] arr = {1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4, 5, 5, 5, 5, 5};
         //int[] arr = {1,2};
-        System.out.println(new _26Remove_Duplicates_from_Sorted_Array().removeDuplicates(arr));
+        System.out.println(new _26_Remove_Duplicates_from_Sorted_Array().removeDuplicates3(arr));
         System.out.println(Arrays.toString(arr));
     }
 
@@ -36,5 +36,37 @@ public class _26Remove_Duplicates_from_Sorted_Array {
             len = 1;
         }
         return len;
+    }
+
+    /**
+     * 另一种写法，直接用一个变量记住可以替换的位置
+     */
+    public int removeDuplicates2(int[] nums) {
+        int len = 0;
+        int p = 0;
+        int i = 0;
+        if (nums != null && nums.length > 1) {
+            while (i < nums.length) {
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                } else {
+                    len++;
+                    nums[p++] = nums[i];
+                }
+                i++;
+            }
+        } else if (nums.length == 1) {
+            len = 1;
+        }
+        return len;
+    }
+
+    public int removeDuplicates3(int[] nums) {
+        int i = 0;
+        for (int x : nums) {
+            if (i == 0 || x > nums[i - 1]) {
+                nums[i++] = x;
+            }
+        }
+        return i;
     }
 }
